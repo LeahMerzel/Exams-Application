4498bd8735013937a6331eb5848719d828cb3b16
LeahMerzel <leahwalden4@gmail.com>
2023-12-14 14:27:59 +0200

    studentExam repo work doen

--- Exams Application/Repositories/StudentExamRepository.cs
+++ Exams Application/Repositories/StudentExamRepository.cs
@@ -8,6 +8,13 @@ namespace Exams_Application.Repositories
         public StudentExamRepository(DbContext dbContext) : base(dbContext)
         {
         }
+        public int SubmitStudenExamGradeToDb(Guid studentId, int grade) { throw new NotImplementedException(); }
+        public List<Question> ReturnQuestionsFailedInExam(Guid StudentExamId) { throw new NotImplementedException();}
+       
+        //should the following 2 or 3 be part of Client side code?
+        public int GetNumberOfQuestionsDone(int examId) { return 0; }
+        public int GetNumberOfQuestionsRemained(int examId) { return 0; }
 
+        //start Timer when student gets Exam
     }
 }
